5a5c0a21c29589a179cde0c27c93a2d6d017c140
Mika <mika@wappia.se>
2020-05-28 09:04:07 +0800

    Add support for multi-branch

--- index.js
+++ index.js
@@ -6,35 +6,36 @@ import {
   readFileSync,
   realpathSync,
   existsSync,
-} from 'fs'
-import {exec} from 'child_process'
-import tmp from 'tmp'
-import mime from 'mime-types'
+} from "fs"
+import { exec } from "child_process"
+import tmp from "tmp"
+import mime from "mime-types"
 
-const logFile = '/tmp/cdn-deploy.log'
-const configFile = '/tmp/cdn/cdn.json'
-let basePath = '/tmp/cdn'
+const logFile = "/tmp/cdn-deploy.log"
+const configFile = "/tmp/cdn/cdn.json"
+let basePath = "/tmp/cdn"
 let db
 const seen = {}
+let branch
 
 const patternImport = new RegExp(
   /import(?:["'\s]*([\w*${}\n\r\t, ]+)from\s*)?["'\s]["'\s](.*[@\w_-]+)["'\s].*$/,
-  'mg'
+  "mg"
 )
 const patternDImport = new RegExp(
   /import\((?:["'\s]*([\w*{}\n\r\t, ]+)\s*)?["'\s](.*([@\w_-]+))["'\s].*\)$/,
-  'mg'
+  "mg"
 )
-const globalImport = new RegExp(/(cdn-import)\((.+)\)/, 'mg')
+const globalImport = new RegExp(/(cdn-import)\((.+)\)/, "mg")
 
 function loadDb() {
-  const db = JSON.parse(readFileSync(configFile, 'utf8'))
+  const db = JSON.parse(readFileSync(configFile, "utf8"))
   if (!db.files) {
     db.files = {}
   }
   if (db.base) {
     basePath =
-      basePath + '/' + db.base.replace(/^[\./]+/, '').replace(/\/+$/, '')
+      basePath + "/" + db.base.replace(/^[\./]+/, "").replace(/\/+$/, "")
   }
   return db
 }
@@ -46,9 +47,9 @@ function saveDb() {
 function maybeDeploy(file, callTree = []) {
   if (callTree.includes(file)) {
     throw new Error(
-      'Circular dependency detected, which this script has no support for! ' +
+      "Circular dependency detected, which this script has no support for! " +
         JSON.stringify(callTree) +
-        ' detected in adding ' +
+        " detected in adding " +
         file
     )
   }
@@ -63,18 +64,18 @@ function maybeDeploy(file, callTree = []) {
 async function deploy(me, callTree) {
   db.files[me] = db.files[me] || {
     version: 0,
-    hash: '',
+    hash: "",
     dependencies: {},
   }
   const dependencies = await getDependencies(me)
 
   await Promise.all(
-    dependencies.map(dependency => maybeDeploy(dependency.absolute, callTree))
+    dependencies.map((dependency) => maybeDeploy(dependency.absolute, callTree))
   )
   const myHash = await getFileHash(me)
 
   const anyDependencyChanged = dependencies.find(
-    d =>
+    (d) =>
       !db.files[me].dependencies[d.absolute] ||
       db.files[me].dependencies[d.absolute] !== db.files[d.absolute].version
   )
@@ -84,16 +85,16 @@ async function deploy(me, callTree) {
     db.files[me].version++
     db.files[me].hash = myHash
     db.files[me].dependencies = {}
-    dependencies.forEach(d => {
+    dependencies.forEach((d) => {
       db.files[me].dependencies[d.absolute] = db.files[d.absolute].version
     })
 
-    const {path, filename} = await createInjectedTempfile(me)
+    const { path, filename } = await createInjectedTempfile(me)
     const destination =
       db.target +
-      ('/' + me.slice(2, me.lastIndexOf('/')) + '/' + filename).replace(
+      ("/" + me.slice(2, me.lastIndexOf("/")) + "/" + filename).replace(
         /\/\//g,
-        '/'
+        "/"
       )
     let url = destination.slice(db.target.length)
     if (db.targetUrl) {
@@ -110,27 +111,27 @@ async function deploy(me, callTree) {
 }
 
 async function createInjectedTempfile(file) {
-  let contents = await readFileSync(`${basePath}/${file}`, 'utf8')
+  let contents = await readFileSync(`${basePath}/${file}`, "utf8")
 
-  ;(await getDependencies(file)).forEach(({raw, absolute}) => {
+  ;(await getDependencies(file)).forEach(({ raw, absolute }) => {
     const version = db.files[file].dependencies[absolute]
-    let index = raw.lastIndexOf('.')
+    let index = raw.lastIndexOf(".")
     if (index < 0) {
       index = raw.length - 1
     }
     contents = contents
       .split(raw)
-      .join(raw.slice(0, index) + '-' + version + raw.slice(index))
+      .join(raw.slice(0, index) + "-" + version + raw.slice(index))
   })
 
-  const filename = file.slice(file.lastIndexOf('/') + 1)
+  const filename = file.slice(file.lastIndexOf("/") + 1)
   const newFilename =
-    filename.slice(0, filename.lastIndexOf('.')) +
-    '-' +
+    filename.slice(0, filename.lastIndexOf(".")) +
+    "-" +
     db.files[file].version +
-    filename.slice(filename.lastIndexOf('.'))
+    filename.slice(filename.lastIndexOf("."))
 
-  const tmpObj = tmp.fileSync({postfix: '--' + newFilename})
+  const tmpObj = tmp.fileSync({ postfix: "--" + newFilename })
   writeFileSync(tmpObj.fd, contents)
   return {
     path: tmpObj.name,
@@ -139,19 +140,28 @@ async function createInjectedTempfile(file) {
 }
 
 async function upload(source, destination, hash, url) {
-  const mimetype = mime.lookup(source) || 'application/octet-stream'
+  const mimetype = mime.lookup(source) || "application/octet-stream"
 
-  await cmd(`cat "${source}" | gzip | gsutil cp - "${destination}"`)
+  try {
+    await cmd(`cat "${source}" | gzip | gsutil cp - "${destination}"`)
+  } catch (e) {
+    if (e.includes("does not have storage.objects.delete access")) {
+      // this file was already uploaded, probably from an interrupted earlier run,
+      // keep working
+    } else {
+      throw e
+    }
+  }
 
   // gsutil returns before it is ready, so give it some time to rest
-  await new Promise(res => setTimeout(res, 10))
+  await new Promise((res) => setTimeout(res, 10))
 
   await retrycmd(`
     gsutil setmeta \
       -h "Cache-Control: public, max-age=31536000" \
       -h "Content-Encoding: gzip" \
       -h "Content-Type: ${mimetype}" \
-      "${destination}" 
+      "${destination}"
   `)
   await retrycmd(`gsutil acl ch -u AllUsers:R "${destination}"`)
 
@@ -167,15 +177,15 @@ async function getDependencies(file) {
     return getDependencies.cache[file]
   }
 
-  const js = readFileSync(`${basePath}/${file}`, 'utf8')
+  const js = readFileSync(`${basePath}/${file}`, "utf8")
 
   const imports = [
     ...js.matchAll(patternImport),
     ...js.matchAll(patternDImport),
     ...js.matchAll(globalImport),
   ]
-    .filter(m => m[2].startsWith('.'))
-    .map(m => ({
+    .filter((m) => m[2].startsWith("."))
+    .map((m) => ({
       raw: m[2],
       absolute: absPath(`${basePath}/${file}/../${m[2]}`),
     }))
@@ -186,7 +196,9 @@ async function getDependencies(file) {
 }
 
 function getFileHash(file) {
-  return cmd(`git rev-parse --short=1 $(git rev-list -1 master -- "${file}")`)
+  return cmd(
+    `git rev-parse --short=1 $(git rev-list -1 ${branch} -- "${file}")`
+  )
 }
 
 async function retrycmd(...args) {
@@ -197,7 +209,7 @@ async function retrycmd(...args) {
       console.error(
         `++++ gsutil failed as usual, but no worries we retry the call in 100ms: ${e.message} (${args[0]})`
       )
-      await new Promise(res => setTimeout(res, 100))
+      await new Promise((res) => setTimeout(res, 100))
     }
   }
 }
@@ -205,7 +217,7 @@ async function retrycmd(...args) {
 let workers = 0
 const queue = []
 async function cmd(str, nocwd = false) {
-  await new Promise(res => {
+  await new Promise((res) => {
     queue.push(res)
   })
 
@@ -220,14 +232,14 @@ async function cmd(str, nocwd = false) {
 
     return await new Promise((res, reject) => {
       let printstr = str.trim()
-      while (printstr.indexOf('  ') !== -1) {
-        printstr = printstr.replace(/  /g, ' ')
+      while (printstr.indexOf("  ") !== -1) {
+        printstr = printstr.replace(/  /g, " ")
       }
       log(printstr)
 
       exec(str, opts, (error, stdout, stderr) => {
         if (error) {
-          reject(printstr + ': ' + stderr.trim())
+          reject(printstr + ": " + stderr.trim())
         } else {
           res(stdout.trim())
         }
@@ -244,30 +256,32 @@ function checkQueue() {
 }
 
 function absPath(file) {
-  const ret = '.' + realpathSync(file).substr(basePath.length)
+  const ret = "." + realpathSync(file).substr(basePath.length)
   return ret
 }
 
 function log(str) {
-  appendFileSync(logFile, str + '\n')
+  appendFileSync(logFile, str + "\n")
 }
 
 async function start() {
-  log('======= NEW DEPLOY: ' + process.cwd())
+  log("======= NEW DEPLOY: " + process.cwd())
   try {
     setInterval(checkQueue, 5)
     if (existsSync(basePath)) {
       await cmd(`git worktree remove /tmp/cdn`, true)
     }
 
+    branch = await cmd(`git rev-parse --symbolic-full-name --abbrev-ref HEAD`, true)
+
     await cmd(`git fetch && git push`, true)
     await cmd(`git worktree add /tmp/cdn`, true)
-    await cmd(`git reset --hard origin/master`)
+    await cmd(`git reset --hard origin/${branch}`)
     db = await loadDb()
     await maybeDeploy(absPath(`${basePath}/${db.entry}`))
     saveDb()
     await cmd(
-      `git add ${configFile} ; git commit -m 'CDN' ; git push origin cdn:master`
+      `git add ${configFile} ; git commit -m 'CDN' ; git push origin cdn:${branch}`
     )
     await cmd(`git worktree remove /tmp/cdn`, true)
   } catch (e) {
